2af0618e77649615ff7a1712de6700e13a9bbb71
Martyn van Dijke <martijnvdijke600@gmail.com>
2026-04-16 09:58:35 +0200

    feat: add Swagger/OpenAPI documentation for the API
    - Add swagger.json with full OpenAPI 3.0 specification
    - Add /docs endpoint with Swagger UI
    - Add /api-docs endpoint for the JSON spec
    - Link API docs in header navigation on both pages

--- main.go
+++ main.go
@@ -262,6 +262,35 @@ func main() {
 		getRandomQuote(w, r)
 	})
 
+	http.HandleFunc("/api-docs", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(basePath, "static/swagger.json"))
+	})
+
+	http.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<!DOCTYPE html>
+<html>
+<head>
+    <title>HEAT Racing API Documentation</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script>
+        window.onload = function() {
+            SwaggerUIBundle({
+                url: "/api-docs",
+                dom_id: '#swagger-ui',
+                presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
+                layout: "BaseLayout"
+            });
+        };
+    </script>
+</body>
+</html>`)
+	})
+
 	http.HandleFunc("/login.html", func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("session")
 		if err == nil {
